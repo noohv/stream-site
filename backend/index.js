de84acfdf611f1c4a4f847d58e58a1ff0ac42d1a
noohv <valters.huuns@gmail.com>
2024-01-28 23:48:10 +0200

    Basic websocket

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
 
+app.use(cors())
 
 io.on('connection', (socket) => {
-	console.log("USER JOINED")
+	console.log("User joined")
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+
+  socket.on('message', (msg) => {
+    console.log(`Message: ${msg}`)
+    io.emit(`message`, msg)
+  })
+
 })
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 import './App.css'
 
-function App() {
+const socket = io('http://localhost:3000')
 
-  return (
+function App() {
+  const [messages, setMessages] = useState<String[]>([])
+  const [inputMessage, setInputMessage] = useState('')
+
+  useEffect(()=> {
+    socket.on('message', (msg) => {
+      setMessages([...messages, msg])
+    })
+  },[messages])
+
+  const handleSendMessage = () => {
+    socket.emit('message', inputMessage)
+    setInputMessage('')
+  }
+
+  return (  
     <>
-      
+      <div>
+        <ul>
+          {messages.map((msg, index) => (
+            <li key={index}>{msg}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </>
   )
 }
